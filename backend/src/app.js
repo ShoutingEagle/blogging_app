8a522b4baadc6c9f44bb4276c55a9fe00df01304
Sameer Paul <sameerpaul58@gmail.com>
2025-05-07 10:48:47 +0530

    cors error fixing

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN ,
-    credentials: true
+    credentials: false
 }))
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
