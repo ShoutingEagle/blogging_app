40bff0423aef695b3ad60b9a688e2de16ab53c4c
Sameer Paul <sameerpaul58@gmail.com>
2025-05-07 11:17:10 +0530

    cors error fixing

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,10 +8,23 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN ,
+const allowedOrigins = [
+    "http://localhost:5173",         // Vite dev
+    "https://your-frontend.vercel.app" // your deployed frontend on Vercel
+  ];
+
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true
-}))
+  }));
+
+  
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(cookieParser());
