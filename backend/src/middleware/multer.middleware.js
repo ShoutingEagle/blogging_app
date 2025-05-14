e1a5cd99d76a46cd35c7294fac119f4011bcb7c6
Sameer Paul <sameerpaul58@gmail.com>
2025-05-15 04:24:21 +0530

    bug fixed multer

--- backend/src/middleware/multer.middleware.js
+++ backend/src/middleware/multer.middleware.js
@@ -1,24 +1,28 @@
-import multer from "multer"
-import fs from "fs"
-import path from "path"
+import multer from "multer";
+import fs from "fs";
+import path from "path";
 
-const tempDir = path.resolve("public/temp")
+const tempDir = path.resolve("public/temp");
 
-if(!fs.existsSync(tempDir)){
-    fs.mkdir(tempDir,{recursive:true})
-    console.log("✅ Created missing folder: public/temp");
+// Ensure the folder exists
+try {
+    if (!fs.existsSync(tempDir)) {
+        fs.mkdirSync(tempDir, { recursive: true });
+        console.log("✅ Created missing folder: public/temp");
+    }
+} catch (error) {
+    console.error("❌ Error creating upload folder:", error);
 }
 
 const storage = multer.diskStorage({
-    destination : function(req,file,cb) {
-        console.log("file => ",file)
-        cb(null,"./public/temp")
+    destination: function (req, file, cb) {
+        cb(null, tempDir);
     },
-    filename : function (req,file,cb) {
-        cb(null,file.originalname)
-    }
-})
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
 
-const upload = multer({storage})
+const upload = multer({ storage });
 
-export default upload
+export default upload;
